24ad096bc78db0f0996958418b285851dd409f65
Alessandro Afloarei <alessandro.afloarei@aflsolutions.it>
2024-07-08 22:56:27 +0200

    Fix sanitizer

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ class SanitizeTool {
 
   static get sanitize() {
     return {
-      span: {
+      annotation: {
         class: true,
-        'data-formula': true,
         contenteditable: true,
         style: true,
         'data-author': true,
@@ -17,7 +16,28 @@ class SanitizeTool {
         'data-volume': true,
         'data-volume-initial-page': true,
         'data-volume-last-page': true,
+        encoding: true,
       },
+      latex: {
+        contenteditable: true,
+        style: true,
+      },
+      span: function (el) {
+        return (
+          el.classList.contains('katex') ||
+          el.classList.contains('katex-mathml') ||
+          el.classList.contains('katex-html') ||
+          el.classList.contains('base') ||
+          el.classList.contains('strut') ||
+          el.classList.contains('mord') ||
+          el.classList.contains('afl-inline-latex') ||
+          el.classList.length === 0
+        );
+      },
+      math: true,
+      semantics: true,
+      mrow: true,
+      mi: true,
     };
   }
 
